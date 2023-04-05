cc7d8ef518f239a17f18a360f26971f910b7e7c4
KaaVii <daniloventurini2112@gmail.com>
2023-04-05 01:40:34 -0300

    First Commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Another Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'This is the third right?',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    }
+]
