14bca50571cc6b16dac197a973c3b7ef7fc739c2
KaaVii <daniloventurini2112@gmail.com>
2023-04-05 12:52:23 -0300

    Add some tests to the uiService

--- src/app/services/ui.service.spec.ts
+++ src/app/services/ui.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UiService } from './ui.service';
 
 describe('UiService', () => {
@@ -12,5 +11,25 @@ describe('UiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initially set showAddText to false', () => {
+    expect(service['showAddText']).toBeFalse();
+  });
+
+  it('should toggle showAddText when toggleAddTask is called', () => {
+    expect(service['showAddText']).toBeFalse();
+    service.toggleAddTask();
+    expect(service['showAddText']).toBeTrue();
+    service.toggleAddTask();
+    expect(service['showAddText']).toBeFalse();
+  });
+
+  it('should emit a value through the subject when toggleAddTask is called', (done: DoneFn) => {
+    service.onToggle().subscribe((value: boolean) => {
+      expect(value).toBeTrue();
+      done();
+    });
+    service.toggleAddTask();
   });
 });
